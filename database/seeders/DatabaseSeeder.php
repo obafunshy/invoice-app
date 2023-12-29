9bffde187d47331d36bfd85617439522580eed11
Oluwaseun F <pharma4mee@yahoo.com>
2023-12-29 18:20:11 +0000

    project successfully setup with fake data seeded

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Counter;
+use App\Models\Product;
+use App\Models\Customer;
+use App\Models\InvoiceItem;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Counter::factory(1)->create();
+        // Product::factory(5)->create();
+        // Customer::factory(5)->create();
+        // InvoiceItem::factory(5)->create();
     }
 }
